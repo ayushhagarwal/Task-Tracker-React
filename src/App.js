fd9a67ac23109d66fce973332cdc2beeda2df4eb
ayushhagarwal <ayushgrwl365@gmail.com>
2021-10-05 21:40:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Header from './Components/Header/Header'
+import Tasks from './Components/Tasks/Tasks';
 
-function App() {
+const App=()=>{
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at school",
+      day: "Feb 6th at 1:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "Feb 5th at 2:30pm",
+      reminder: false,
+    },
+  ]);
+
+  //Delete Task
+  const deleteTask=(id)=>{
+    setTasks(tasks.filter((task)=>task.id!==id))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <h1>Hello, react app!</h1>
+      <Header title="Header title"/>
+      {tasks.length>0?(
+        <Tasks tasks={tasks} onDelete={deleteTask}/>
+      ):(
+        'No Tasks, Please add one!'
+      )}
+      
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+import Button from "../Button/Button";
+
+const Header = ({ title }) => {
+  const onClick = () => {
+    console.log("click");
+  };
+  return (
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "if No prop passed",
+};
+
+Header.propTypes = {
+  title: PropTypes.string,
+};
+export default Header;
