94ffcb38085a1e2dc67a783e138bddff7d1c18af
ayushhagarwal <ayushgrwl365@gmail.com>
2021-10-07 21:50:03 +0530

    including sudo in .sh file

--- host.sh
+++ host.sh
@@ -1,12 +1,14 @@
-sed -i 's/\r$//' ./scripts/*
-chmod +x ./scripts/*
-
 # Install Docker 
-./scripts/dockerinstall.sh
+sed -i 's/\r$//' ./scripts/dockerinstall.sh
+chmod 7777 ./scripts/dockerinstall.sh
+sudo ./scripts/dockerinstall.sh
 
-#build the container 
-./scripts/build.sh
+#build the container
+sed -i 's/\r$//' ./scripts/buil.sh
+chmod 7777 ./scripts/buil.sh 
+sudo ./scripts/build.sh
 
 #deploy the container
-
-./scripts/deploy.sh
+sed -i 's/\r$//' ./scripts/deploy.sh
+chmod 7777 ./scripts/deploy.sh
+sudo ./scripts/deploy.sh
